89a74e1cea04aec7acf14188572058c4e12907f8
Isaac <isaac.benites@koin.com.br>
2020-05-02 12:21:05 -0300

    Trabalhando com tipos genéricos

--- app/js/views/View.js
+++ app/js/views/View.js
@@ -2,4 +2,10 @@ class View {
     constructor(seletor) {
         this.elemento = document.querySelector(seletor);
     }
+    update(modelo) {
+        this.elemento.innerHTML = this.template(modelo);
+    }
+    template(modelo) {
+        throw new Error('Implemente o método template!');
+    }
 }
--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,10 +1,17 @@
-class View{
+class View<T> {
 
     protected elemento : Element;
 
-    constructor(seletor : string){
-
+    constructor(seletor:string){
         this.elemento = document.querySelector(seletor);
+    }
 
+    update(modelo:T) : void {
+        this.elemento.innerHTML = this.template(modelo);
+    }
+
+    template(modelo:T): string {
+
+        throw new Error('Implemente o método template!')
     }
 }
